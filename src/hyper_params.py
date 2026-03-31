1eb87dd659c456ff67ed431f2fc31c753b3ddbab
tauhidhasan811 <tauhidhasan811@gmail.com>
2026-03-31 14:34:26 +0600

    add worflow ids and parameters

--- src/hyper_params.py
+++ src/hyper_params.py
@@ -10,9 +10,27 @@ headers = {
 }
 
 params = {
-    "workflow_id": ["69ca4c4689c6022432b26c89"],
-    "workflow_base_url": {
-        "run": "https://api.mindpal.io/api/v2/workflow/run?workflow_id=",
-        
+    "workflow_list": {
+        "personality_and_interest": "69cb3e0c6b30038ca03a1b90",
+        "learning_style": "69cb3a136b30038ca03a1b65",
+        "personal_ability": "69ca4c4689c6022432b26c89",
+        "idp": "69cb4e9b234d4920c169871c",
+        "idp-pd": "69cb5159234d4920c1698738",
+        "ifp": "69cb568ca66602ff1552139a",
+        "ifp-pd": "69cb6777289bc826a836fdf5"
+    },
+    "workflow": {
+        "run": {
+            "url": "https://api.mindpal.io/api/v2/workflow/run?workflow_id=",
+            "method": "POST"
+        },
+        "status": {
+            "url": "https://api.mindpal.io/api/workflow-run-status/retrieve-by-id?run_id=",
+            "method": "GET"
+        },
+        "result": {
+            "url": "https://api.mindpal.io/api/workflow-run-result/retrieve-by-id?run_id=",
+            "method": "GET"
+        }
     }
 }
